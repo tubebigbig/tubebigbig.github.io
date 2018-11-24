dbe0519e95cf8f3231f41fe5222dee67116a19be
tubebigbig <45288256+tubebigbig@users.noreply.github.com>
2018-11-25 00:40:37 +0800

    Add files via upload

--- scripts/main.js
+++ scripts/main.js
@@ -12,22 +12,3 @@ myImage.onclick = function() {
     
 }
 
-var myButton = document.querySelector('button');
-var myHeading = document.querySelector('h1');
-
-function setUserName(){
-    var myName = prompt('please enter your name');
-    localStorage.setItem('name',myName);
-    myHeading.innerHTML = 'this is my html,' + myName;
-}
-
-if(!localStorage.getItem('name')){
-    setUserName();
-} else {
-    var storedName = localStorage.getItem('name');
-    myHeading.innerHTML = 'this is my html,' + storedName;
-}
-
-myButton.onclick = function(){
-    setUserName();
-}
